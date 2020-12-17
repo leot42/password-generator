853d22008e5fa0056eb2a14aadb2f23e2da42418
Lionel Townsend <townsendlionel@gmail.com>
2020-12-17 13:54:23 -0600

    created single char generation logic

--- script.js
+++ script.js
@@ -1,5 +1,16 @@
 // Assignment code here
-
+function getRandomIntInclusive(min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min + 1) + min); //The maximum is inclusive and the minimum is inclusive
+}
+  function returnRandomChar(firstCharInRange, lastCharInRange){
+    return String.fromCharCode(getRandomIntInclusive(firstCharInRange, lastCharInRange));
+  };
+function generatePassword() {
+   return returnRandomChar(33, 47);
+}
+// console.log(getRandomInt(65, 90)); 
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
