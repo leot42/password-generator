10813711d8abb9cd4caab88a3cfc32432f4ade2b
Lionel Townsend <townsendlionel@gmail.com>
2021-01-02 20:15:46 -0600

    ui functional but could be refactored

--- script.js
+++ script.js
@@ -9,7 +9,7 @@
  * @param {number} options The number representing the options choice.
  * @returns {boolean} I the option valid for this question.
  */
-function passwordModelLogic (options) {
+function passwordLengthModelLogic (options) {
   this.options = options;
   if (this.options > 7 && this.options <= 128) { // User answer was a valid option.
     console.log(`The user value of ${options} is between 7 and 129.`);
@@ -21,12 +21,32 @@ function passwordModelLogic (options) {
     alert(`Sorry.\n\n    ${options} was not a valid option. \n\nNo problem... \n\nLets try again!`)
     return false}
 };
+
+function passwordCharTypeModelLogic (options) {
+  let isUserOptionsAnswerValid = true;
+  // check options for validity
+  if (parseInt(options) === null || parseInt(options) === NaN) { isUserOptionsAnswerValid = false;}
+
+  this.optionsArr = Array.from(options);
+  this.optionsArr.forEach(element => {
+    if (parseInt(element) === 1) {userChoiceData.charTypeUppercase = true;} // Use uppercase.
+    if (parseInt(element) === 2) {userChoiceData.charTypeLowercase = true;} // Use lowercase.
+    if (parseInt(element) === 3) {userChoiceData.charTypeNumbers = true;} // Use numeric.
+    if (parseInt(element) === 4) {userChoiceData.charTypeSymbols = true;} // Use special characters.
+    console.log("passwordCharTypeModelLogic -> userChoiceData -> userChoiceData modified");
+  });
+  // mark userChoice process as complete
+  userChoiceData.isComplete = true;
+  return isUserOptionsAnswerValid;
+};
+
 let userChoiceData = {
   passwordLength: 999,
   charTypeUppercase: false,
   charTypeLowercase: false,
   charTypeNumbers: false,
-  charTypeSymbols: false
+  charTypeSymbols: false,
+  isComplete: false
 }
 
 
@@ -50,8 +70,10 @@ function isValidNumber(numberToCheck) {
 
 /**Structure of the page text and validation logic. */
 class PageTextStructure {
-  
+  /**@type {pageContext} The context that this page structure exists within. */
+  pageContextRef;
   #userAnswer;
+  #nextStateWhenCurrentIsComplete
   /**
    * 
    * @param {*} titleText The header of the page to display to the user.
@@ -59,13 +81,15 @@ class PageTextStructure {
    * @param {*} invalidAnswerText The message to display when the user input was invalid.
    * @param {*} exampleAnswerText Text to give the user examples to follow.
    * @param {*} validationLogic Model to validate user input.
+   * @param {PageTextStructure} nextState The next state if when all requirements have been met.
    */
-  constructor(titleText, choiceText, invalidAnswerText, exampleAnswerText, validationLogic) {
+  constructor(titleText, choiceText, invalidAnswerText, exampleAnswerText, validationLogic, nextState) {
     this.titleText = titleText + "\n\n";
     this.choiceText = choiceText + "\n";
     this.invalidAnswerText = invalidAnswerText + "\n";
     this.exampleAnswerText = exampleAnswerText + "\n";
     this.validationLogic = validationLogic;
+    this.nextState = nextState;
   }
 
   run() {
@@ -83,7 +107,7 @@ class PageTextStructure {
     this.verifyAnswerWasIntended();
 
     // save answer to outside model.
-    throw "State permanence not completed. Need to decouple from global and add as a parameter. "
+   // throw "State permanence not completed. Need to decouple from global and add as a parameter. "
 
   }
 
@@ -107,7 +131,6 @@ class PageTextStructure {
 
     // check to see if answer is an valid answer.
 
- 
   }
 
   askQuestion() {
@@ -142,18 +165,19 @@ class PageTextStructure {
         // answer was not a valid option
         isUserAnswerValid = false;
         console.log(`User confirmation answer was '${userAnswer}' which was not a '1' or '2'.`);
+        alert(`Whoops...\n\n    It seems "${this.#userAnswer}" wasn't entirely a number or was an incorrect option.\n\n    That's okay.\n\nLet's try again.`)
+        this.run();
       }
 
     }
 
     // ask the user the question untill they give a valid answer and confirm their input is correct..
-    while (isUserAnswerValid = false || userAnswerNumber === 2) {
+    while (isUserAnswerValid === false || userAnswerNumber === 2) {
       // if user answer is valid but is not intended apologize 
       if (isUserAnswerValid) {
         alert(`Whoops...\n\n    It seems ${userAnswerNumber} wasn't the input that was intended.\n\n    That's okay.\n\nLet's try again.`)
       }
       this.run()
-      // throw "User invald confirmation answer question re-ask not impremented."
     }
 
     if (isUserAnswerValid === true) {
@@ -161,10 +185,12 @@ class PageTextStructure {
       userChoiceData.passwordLength = this.#userAnswer;
 
       //change context to next question
-      throw "Context change not implemented for user entering an intended answer.";
+      this.pageContextRef.change(passwordCharTypeQuestionPage);
+      //throw "Context change not implemented for user entering an intended answer.";
     }
     else {
       throw "Context change not implemented for user entering an un-intended answer.";
+      // alert(`Whoops...\n\n    It seems ${userAnswerNumber} wasn't the input that was intended.\n\n    That's okay.\n\nLet's try again.`)
 
     }
   }
@@ -172,6 +198,8 @@ class PageTextStructure {
   isNumberCheckTrue() {
     if(isValidNumber(this.#userAnswer) === true){console.log("was a number"); return;}
     else {
+      alert(`Whoops...\n\n    It seems "${this.#userAnswer}" wasn't entirely a number.\n\n    That's okay.\n\nLet's try again.`)
+      this.run();
       throw "Answer is not a number handling is not implemented."
       // user answer is not a number
       //give the user an error message and reissue the question
@@ -186,22 +214,26 @@ class PageTextStructure {
   }
 }
 // Page PageTextStructure instances
-let passwordLengthQuestionPage = new PageTextStructure(
+var passwordLengthQuestionPage = new PageTextStructure(
   "Choose Password Length\n",
   "Password length can from 8 to 128 characters long.\n",
   "I sorry that was an invalid answer.\n",
   "Example 8 or 99 or 128\n",
-  passwordModelLogic
+  passwordLengthModelLogic,
+  passwordCharTypeQuestionPage
 );
 
-let passwordCharTypeQuestionPage = new PageTextStructure(
+var passwordCharTypeQuestionPage = new PageTextStructure(
   "Choose Character Types Used\n",
   "The options for the types are:\n\n1: Uppercase\n    ex: JKAX\n\n2: Lowercase\nex: tbiqz\n\n3: Numeric\n    ex: 49706\n\n4: Special Characters\n    ex: *_.\"?\) \n",
   "I sorry that was an invalid answer.\n",
-  "Example 8 or 99 or 128\n",
-  "",
-  
+  "Example 1234 or 24 or 124\n",
+  passwordCharTypeModelLogic,
+  // @ts-ignore
+  generateRandomChar
 ) 
+
+
 
 /**@summary Valdates user input as a number.
  * @param {string} userPromptOutput Text recieved from the prompt given by the user.
@@ -390,12 +422,19 @@ let passwordCharTypePageModel = {"isUpperCase":false, "isLowerCase": false, "isN
 // //#endregion
 /**@param {PageTextStructure} startingState The default starting page for the state machine.*/
 function pageContext(startingState) {
+  startingState.pageContextRef = this;
    let currentState = startingState;
 
 /** summary Called by a stateObj to change the state within the page context which will be called later by the this.start method.
  * @param {PageTextStructure} stateObj Represents a questionPage and it's validation.
 */
-  this.change = function (stateObj){currentState = stateObj;}
+  this.change = function (stateObj){
+    // if the userChoiceData is complete 
+    if (userChoiceData.isComplete === true) {return}
+    currentState = stateObj
+    currentState.pageContextRef = this;
+    currentState.run();
+  }
 
 /**@summary Allows the currentState to call out to the pageContext in order to run the currentState property */
   this.start = function () {currentState.run();}
@@ -409,15 +448,12 @@ function pageContext(startingState) {
 
 // call question page that returns a valid answer
 function generatePassword() {
-//Get password length
-//passwordLengthQuestionPage.askQuestion(); // maybe should be named .getAnswer() method
 
-//todo Add pageContext
+// pageContext
 var pageContextInst = new pageContext(passwordLengthQuestionPage);
 pageContextInst.start();
 //Choose chars types
-/**@todo */ //chooseCharTypesPage.askQuestion();
-
+console.log(userChoiceData);
   return generateRandomChar(symbolsCharType.getCharRangeArrayContents);
 }
 
